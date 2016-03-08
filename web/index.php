a548035822dec881d26b39f6906358037ae8ee1c
Andrew Shell <andrew@andrewshell.org>
2016-03-08 13:51:20 -0600

    Testing a switch from Radar to Silex

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
-use Radar\Adr\Boot;
-use Zend\Diactoros\Response as Response;
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\Request;
+use Zend\Diactoros\Response as PsrResponse;
 use Zend\Diactoros\ServerRequestFactory as ServerRequestFactory;
 
 ini_set('display_errors', true);
@@ -10,12 +12,34 @@ ini_set('display_errors', true);
  */
 require '../vendor/autoload.php';
 
-$boot = new Boot();
-$adr = $boot->adr([
-    'Blog\Delivery\Config',
-]);
+$app = new Silex\Application();
+$app['debug'] = true;
 
-/**
- * Run
- */
-$adr->run(ServerRequestFactory::fromGlobals(), new Response());
+$app['postGateway'] = function () {
+    $postGateway = new \Blog\Data\Gateway\PostMemory();
+    $postGateway->savePost(new \Blog\Domain\Entity\Post('Sample Post 1', 'This is the first sample post.', '', '1'));
+    $postGateway->savePost(new \Blog\Domain\Entity\Post('Sample Post 2', 'This is the second sample post.', '', '2'));
+    $postGateway->savePost(new \Blog\Domain\Entity\Post('Sample Post 3', 'This is the third sample post.', '', '3'));
+
+    return $postGateway;
+};
+
+$app->get('/', function (Request $request) use ($app) {
+    $psr7Factory = new DiactorosFactory();
+    $psrRequest = $psr7Factory->createRequest($request);
+    $psrResponse = new PsrResponse();
+
+    $input = new \Blog\Delivery\Input\NoneExpected();
+    $action = new \Blog\Domain\Interactor\ListAllPosts($app['postGateway']);
+    $responder = new \Blog\Delivery\Responder\Html(__DIR__ . '/../views');
+
+    $psrRequest = $psrRequest->withAttribute('_view', 'listposts.html.php');
+    $payload = $action($input($psrRequest));
+
+    $psrResponse = $responder($psrRequest, $psrResponse, $payload);
+
+    $httpFoundationFactory = new HttpFoundationFactory();
+    return $httpFoundationFactory->createResponse($psrResponse);
+});
+
+$app->run();
